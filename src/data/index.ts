0634e0e7059cc0c6cdf682dcb2907a8ed49e250a
NicolasOBP <nicolasobp15@gmail.com>
2026-04-03 12:00:51 -0300

    feat: add mock data and types for product listings

---
+++ src/data/index.ts
@@ -0,0 +1,2 @@
+export { PRODUCTS_DATA } from "./mockData";
+export { Product } from "./types";
---
+++ src/data/mockData.ts
@@ -0,0 +1,34 @@
+import { Product } from "./types";
+
+export const PRODUCTS_DATA: Product[] = [
+  {
+    id: "1",
+    title: "Gasolina Premium",
+    price: 5.99,
+  },
+  {
+    id: "2",
+    title: "Gasolina Comum",
+    price: 4.99,
+  },
+  {
+    id: "3",
+    title: "Diesel",
+    price: 5.49,
+  },
+  {
+    id: "4",
+    title: "Etanol",
+    price: 3.99,
+  },
+  {
+    id: "5",
+    title: "Etanol Premium",
+    price: 4.99,
+  },
+  {
+    id: "6",
+    title: "Diesel S10",
+    price: 7.99,
+  },
+];
