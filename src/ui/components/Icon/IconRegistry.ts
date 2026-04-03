ef0dc751a5c4295c1daacb2a6d06018586788821
NicolasOBP <nicolasobp15@gmail.com>
2026-04-03 11:58:15 -0300

    feat: add SearchIcon component and update IconRegistry to include it

--- src/ui/components/Icon/IconRegistry.ts
+++ src/ui/components/Icon/IconRegistry.ts
@@ -1,9 +1,11 @@
-import { EyeOffIcon } from "assets/icons/EyeOffIcon";
-import { EyeOnIcon } from "assets/icons/EyeOnIcon";
+import { EyeOffIcon } from "../../../../assets/icons/EyeOffIcon";
+import { EyeOnIcon } from "../../../../assets/icons/EyeOnIcon";
+import { SearchIcon } from "../../../../assets/icons/SearchIcon";
 
 export const iconRegistry = {
   eyeOn: EyeOnIcon,
   eyeOff: EyeOffIcon,
+  search: SearchIcon,
 };
 
 export type IconType = typeof iconRegistry;
