7e1b95aa2fb1cea5680bbe76d8e2346eb564bfd3
NicolasOBP <nicolasobp15@gmail.com>
2026-05-04 11:02:43 -0300

    refactor: simplify useProductsList by removing unused imports and useEffect

--- src/domain/product/useCases/useProductsList.ts
+++ src/domain/product/useCases/useProductsList.ts
@@ -1,23 +1,15 @@
-import { useEffect } from "react";
-
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 import { QueryKeys, useRepository } from "@infra";
 
 export function useProductsList(searchDebounced: string) {
   const { product } = useRepository();
-  const queryClient = useQueryClient();
 
   const { data, isLoading, isError, refetch, isFetching } = useQuery({
-    queryKey: [QueryKeys.ProductList],
+    queryKey: [QueryKeys.ProductList, searchDebounced],
     queryFn: () => product.list(searchDebounced),
     staleTime: 1000 * 30,
   });
-
-  useEffect(() => {
-    queryClient.invalidateQueries({ queryKey: [QueryKeys.ProductList] });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchDebounced]);
 
   return {
     products: data,
