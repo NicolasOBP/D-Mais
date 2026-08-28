2e98b6ee60e76a19f2ee75d004dab9f590c48159
NicolasOBP <nicolasobp15@gmail.com>
2026-08-28 11:24:09 -0300

    feat: refactor ProductModalBody to improve structure and integrate inventory dropdown

--- src/ui/components/ProductModalBody/ProductModalBody.tsx
+++ src/ui/components/ProductModalBody/ProductModalBody.tsx
@@ -1,23 +1,29 @@
-// eslint-disable-next-line import/named
-import { FieldValues } from "react-hook-form";
+import { ProductSchema } from "@schemas";
 
 import { Box } from "@core-components";
 
+import { DropDownTextInput } from "../DropDownTextInput";
 import { ControllerProps, FormTextInput } from "../Form/FormTextInput";
 
-export function ProductModalBody<FormType extends FieldValues>({
+export function ProductModalBody({
   control,
-  name,
-}: ControllerProps<FormType>) {
+}: Pick<ControllerProps<ProductSchema>, "control">) {
   return (
-    <Box alignItems="center">
+    <Box gap="s14" style={{ marginTop: -20 }}>
+      <DropDownTextInput
+        dropdownItems={["inventory1", "inventory2"]}
+        name="inventory"
+        control={control}
+        textFieldStyle={{ paddingVertical: "s8" }}
+        label="Estoque"
+      />
+
       <FormTextInput
         control={control}
-        name={name}
-        boxProps={{ width: 150 }}
-        style={{ textAlign: "center" }}
-        placeholder="Quantidade"
+        name="volume"
         keyboardType="number-pad"
+        label="Litros"
+        textFieldStyle={{ paddingVertical: "s8" }}
       />
     </Box>
   );
