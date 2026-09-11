1d9a225cc4130810aab08493d0a942a3431bbd0e
NicolasOBP <nicolasobp15@gmail.com>
2026-09-11 09:49:00 -0300

    feat: add Inventory icons and update IconRegistry for inventory support

--- src/ui/components/Icon/IconRegistry.ts
+++ src/ui/components/Icon/IconRegistry.ts
@@ -9,6 +9,8 @@ import { EyeOffIcon } from "../../../../assets/icons/EyeOffIcon"
 import { EyeOnIcon } from "../../../../assets/icons/EyeOnIcon"
 import { HomeFillIcon } from "../../../../assets/icons/HomeFillIcon"
 import { HomeIcon } from "../../../../assets/icons/HomeIcon"
+import { InventoryFillIcon } from "../../../../assets/icons/InventoryFillIcon"
+import { InventoryIcon } from "../../../../assets/icons/InventoryIcon"
 import { OrderFillIcon } from "../../../../assets/icons/OrderFillIcon"
 import { OrderIcon } from "../../../../assets/icons/OrderIcon"
 import { PencilIcon } from "../../../../assets/icons/PencilIcon"
@@ -32,6 +34,8 @@ export const iconRegistry = {
 	trash: TrashIcon,
 	chevronRight: ChevronRightIcon,
 	chevronDown: ChevronDownIcon,
+	inventory: InventoryIcon,
+	inventoryFill: InventoryFillIcon,
 }
 
 export type IconType = typeof iconRegistry
